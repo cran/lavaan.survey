4d1dfcf44de62a4302575fd85a95c15eae84bf00
Daniel Oberski <daniel.oberski@gmail.com>
2015-11-09 11:34:18 +0000

    version 1.1.3

--- R/lavaan.survey.R
+++ R/lavaan.survey.R
@@ -1,5 +1,5 @@
 # Complex sampling analysis of SEM models
-# Daniel Oberski, 2013-09-25
+# Daniel Oberski, 2015-11-03
 
 lavaan.survey <- 
   function(lavaan.fit, survey.design, 
@@ -15,30 +15,33 @@ lavaan.survey <-
   ov.names <- lavaanNames(lavaan.fit, type="ov", group=1)
   
   # The MP-inverse duplication matrix is handy for removing redundancy
-  Dplus <- ginv(lavaan::duplicationMatrix(length(ov.names)))
+  Dplus <- lavaan::lav_matrix_duplication_ginv( length(ov.names) )
   # Create a formula that includes all observed variables for svymean
   ov.formula <- as.formula(paste("~", paste(ov.names, collapse="+")))
   
   # <no. group>-sized lists that will contain the asy. covariance matrix,
   #  and sample covariance matrix and mean vector
-  Gamma <- vector("list", lavaan.fit@Data@ngroups)
-  sample.cov <- vector("list", lavaan.fit@Data@ngroups)
-  sample.mean <- vector("list", lavaan.fit@Data@ngroups)
+  ngroups <- lavInspect(lavaan.fit, "ngroups")
   
-  for(g in seq(lavaan.fit@Data@ngroups)) {
-    if(lavaan.fit@Data@ngroups > 1) {
+  Gamma <- vector("list", ngroups)
+  sample.cov <- vector("list", ngroups)
+  sample.mean <- vector("list", ngroups)
+  sample.nobs <- vector("list", ngroups)
+  
+  for(g in seq(ngroups)) {
+    if(ngroups > 1) {
       # Use survey::subset to create data groups
       survey.design.g <- 
         subset(survey.design, eval(parse(text=sprintf("%s == '%s'", 
-                                                      lavaan.fit@call$group, 
-                                                      lavaan.fit@Data@group.label[[g]]))))
+                                                      lavInspect(lavaan.fit, "group"), 
+                                                      lavInspect(lavaan.fit, "group.label")[[g]]))))
     } 
     else { # In case of no groups, just use the original survey design object.
       survey.design.g <- survey.design  
     }
     
     # Function that takes survey design and returns the Gamma & observed moments
-    get.stats.design <- function(survey.design.g, sample.nobs) {
+    get.stats.design <- function(survey.design.g, sample.nobs.g) {
       sample.cov.g <- as.matrix(svyvar(ov.formula, design=survey.design.g, na.rm=TRUE))  
       # survey package returns the variance matrix of the (co)variances as attr:
       Gamma.cov.g <- attr(sample.cov.g, "var")
@@ -51,15 +54,15 @@ lavaan.survey <-
       
       # Join asy. variance matrices for means and covariances
       # TODO add offdiag
-      Gamma.g <- as.matrix(Matrix::bdiag(Gamma.mean.g, Gamma.cov.g))
+      Gamma.g <- lavaan::lav_matrix_bdiag(Gamma.mean.g, Gamma.cov.g)
       
-      Gamma.g <- Gamma.g * sample.nobs[g] # lavaan wants nobs * Gamma.
+      Gamma.g <- Gamma.g * sample.nobs.g # lavaan wants nobs * Gamma.
       
       # Since the above nonparametric estimate of Gamma can be unstable, Yuan
       # and Bentler suggested a model-smoothed estimate of it, optional here:
       if(estimator.gamma == "Yuan-Bentler") {
         r <- get.residuals(lavaan.fit) # Iff these asy = 0, all will be well...
-        Gamma.g <- Gamma.g + (sample.nobs[g]/(sample.nobs[g] - 1)) * (r %*% t(r))
+        Gamma.g <- Gamma.g + (sample.nobs.g/(sample.nobs.g - 1)) * (r %*% t(r))
       }
       # Get rid of attribute, preventing errors caused by lazy evaluation
       # (This has to be at the end or lazy evaluation mayhem will ensue)
@@ -73,20 +76,23 @@ lavaan.survey <-
     # The data may be a list of multiply imputed datasets
     if(!any(class(survey.design.g) == "svyimputationList")) {
       # If no imputations, just use usual no. observations and asy variance
-      sample.nobs <- unlist(lavaan.fit@Data@nobs)
-      stats <- get.stats.design(survey.design.g, sample.nobs)
+      sample.nobs.g <- lavInspect(lavaan.fit, "nobs")[[g]] 
+
+      stats <- get.stats.design(survey.design.g, sample.nobs.g)
     } 
     else { # In case of multiply imputed data
       # Not only can nobs differ from lavaan.fit, but also per imputation
-      sample.nobs <- get.sample.nobs(survey.design.g, lavaan.fit@call$group)
-      # Retrieve point and variance estimates per imputation
-      stats.list <- lapply(survey.design.g[[1]], get.stats.design, sample.nobs=sample.nobs)
+      sample.nobs.g <- get.sample.nobs(survey.design.g, lavInspect(lavaan.fit, "group"))
+      
+      # Retrieve point and variance estimates per imputation, 
+      #    [TODO: this line will not be correct when nobs differs over imputations]
+      stats.list <- lapply(survey.design.g[[1]], get.stats.design, sample.nobs=sample.nobs.g)
       m  <- length(stats.list) # no. imputation
       
       # Point estimates are average over imputations
       sample.cov.list <- lapply(stats.list, `[[`, 'sample.cov.g')
       sample.cov.g <- Reduce(`+`, sample.cov.list) / m
-      cov.df <- Reduce(`rbind`, lapply(sample.cov.list, vech))
+      cov.df <- Reduce(`rbind`, lapply(sample.cov.list, lavaan::lav_matrix_vech))
       sample.mean.list <- lapply(stats.list, `[[`, 'sample.mean.g')
       sample.mean.g <- Reduce(`+`, sample.mean.list) / m
       mean.df <- Reduce(`rbind`, sample.mean.list)
@@ -104,9 +110,10 @@ lavaan.survey <-
     Gamma[[g]] <- stats$Gamma.g
     sample.cov[[g]] <- stats$sample.cov.g
     sample.mean[[g]] <- stats$sample.mean.g
+    sample.nobs[[g]] <- sample.nobs.g
   } # End of loop over groups
 
-  new.call <- lavaan.fit@call
+  new.call <- lavInspect(lavaan.fit, "call")
   new.call$data <- NULL                # Remove any data argument
   new.call$sample.cov <- sample.cov    # Set survey covariances
   new.call$sample.mean <- sample.mean  # Set survey means
@@ -122,7 +129,7 @@ lavaan.survey <-
     # Note that Gamma may be singular.
     new.call$WLS.V <- lapply(Gamma, ginv)
   }
-  new.fit <- eval(new.call) # Run lavaan with the new arguments
+  new.fit <- eval(as.call(new.call)) # Run lavaan with the new arguments
   
   if(estimator %in% c("WLS", "DWLS")) return(new.fit) # We are done for WLS
 
@@ -130,11 +137,11 @@ lavaan.survey <-
   # created dependencies in the parameter estimates.
   # (Code below should really be implemented in lavaan...)
   evs.too.small <- sapply(Gamma, function(Gamma.g) {
-    any(eigen(Gamma.g, only.values=TRUE)$values < .Machine$double.eps*10)
+    any(eigen(Gamma.g, only.values=TRUE)$values < (.Machine$double.eps*100))
   })
   if(any(evs.too.small)) {
-    V.est <- vcov(new.fit)
-    if(any(eigen(V.est, only.values=TRUE)$values < (.Machine$double.eps*10))) {
+    V.est <- lavaan::vcov(new.fit)
+    if(any(Re(eigen(V.est, only.values=TRUE)$values) < (.Machine$double.eps*100))) {
       long.string  <- sprintf("Some of the standard errors may not be trustworthy.
         Some of the observed covariances or means are
         collinear, and this has generated collinearity in your
@@ -154,18 +161,16 @@ lavaan.survey <-
 # Obtain residuals from a lavaan fit object, concatenating means w/ covariances
 # (used in Yuan-Bentler correction)
 get.residuals <- function(fit) {
-    r  <- residuals(fit)
-    c(r$mean, vech(r$cov))
+    r  <- lavaan::residuals(fit)
+    c(r$mean, lavaan::lav_matrix_vech(r$cov))
 }
 
 # Obtain sample size from multiply imputed svydesign object.
 # In case sample size differs over imputations, takes median over imputations.
 # TODO: Does not work with multiple group yet.
 get.sample.nobs  <- function(svy.imp.design, group=NULL) {
-  if(is.null(group)) {
-    nobs.imp <- lapply(svy.imp.design[[1]], function(des) {nrow(des$variables)})
-    return(median(unlist(nobs.imp)))
-  }
+  nobs.imp <- lapply(svy.imp.design[[1]], function(des) {nrow(des$variables)})
+  return(median(unlist(nobs.imp)))
 }
 
 # Use the pFsum function from the survey package to obtain p value for the 
@@ -179,23 +184,15 @@ get.sample.nobs  <- function(svy.imp.design, group=NULL) {
 #   mixture of F's distribution (Skinner, Holt & Smith, pp. 86-87).
 pval.pFsum <- function(lavaan.fit, survey.design, method = "saddlepoint") {
   # Check that Satorra-Bentler or Satterthwaite adjustment is present
-  if(!lavaan.fit@Options$test %in% 
+  if(!lavInspect(lavaan.fit, "options")$test %in% 
               c("satorra.bentler", "mean.var.adjusted", "Satterthwaite")) {
     stop("Please refit the model with Satorra-Bentler (MLM) or Satterthwaite (MLMVS) adjustment.") 
   }
-  test <- lavaan.fit@Fit@test
-  if("UGamma.eigenvalues" %in% names(test[[2]])) {
-    real.eigen.values <- test[[2]]$UGamma.eigenvalues
-    return(survey::pFsum(x=test[[1]]$stat, df=rep(1, length(real.eigen.values)), 
-                  a=real.eigen.values, ddf=degf(survey.design), lower.tail=FALSE,
-                  method=method))
-  } 
-  warning("U Gamma eigenvalues not available from this version of lavaan, defaulting to Satterthwaite method.")
-  if(!lavaan.fit@Options$test %in% c("mean.var.adjusted", "Satterthwaite")) {
-    # (Needed in case eigenvalues are not available)
-    stop("Please refit the model with Satterthwaite (MLMVS) adjustment.") 
-  }
-  # Eigenvalues of U%*%Gamma only available in dev version of lavaan, 
-  #     if n/a use Satterthwaite adjustments (same as method="Satterthwaite"):
-  pf(test[[2]]$stat / test[[2]]$df, df1=test[[2]]$df, df2=degf(survey.design), lower.tail=FALSE)
+  
+  UGamma <- lavTech(lavaan.fit, "ugamma")
+  real.eigen.values <- Re(eigen(lavTech(lavaan.fit, "ugamma"), only.values = TRUE)$values)
+
+  return(survey::pFsum(x=fitMeasures(lavaan.fit, "chisq"), df=rep(1, length(real.eigen.values)), 
+                a=real.eigen.values, ddf=survey::degf(survey.design), lower.tail=FALSE,
+                method=method))
 }
